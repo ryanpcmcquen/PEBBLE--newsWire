66560b01f44c0adeb3c14caa0d7c6a158577024a
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2016-01-02 09:43:51 -0800

    works!

--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
-/* nytWire by ryanpcmcquen v0.1.0 */
+/* nytWire by ryanpcmcquen v1.0 */
 
+/*global require*/
+/*jslint browser:true, white:true*/
 var UI = require('ui');
 var ajax = require('ajax');
 
 var card = new UI.Card({
-  title: 'nytWire',
-  body: 'by Ryan P.C. McQuen',
   scrollable: true
 });
-
-card.show();
 
 ajax({
   url: 'http://api.nytimes.com/svc/news/v3/content/all.json?api-key=9b37b8880161ffb08f95df55513f0ce1:6:73910200',
   type: 'json'
-  }, function(data) {
-    card.title(data.results.title);
-    card.body(data.results.abstract);
-  }
-);
+}, function(data) {
+  var i = 0;
+  card.on('click', function() {
+    i += 1;
+    card.title(data.results[i].title);
+    card.body(data.results[i].abstract);
+  });
+  card.on('longClick', function() {
+    i -= 1;
+    card.title(data.results[i].title);
+    card.body(data.results[i].abstract);
+  });
+  card.title(data.results[i].title);
+  card.body(data.results[i].abstract);
+});
+
+card.show();
