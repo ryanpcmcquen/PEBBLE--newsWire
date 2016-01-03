61b2822e38701b2ce393ea7497381b7c3d71d94e
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2016-01-03 05:46:18 -0800

    making more generic, adding more feeds

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-/* nytWire by ryanpcmcquen v1.0 */
+/* newsWire by ryanpcmcquen v1.0 */
 
 /*global require*/
 /*jslint browser:true, white:true*/
+
 var UI = require('ui');
 var ajax = require('ajax');
 
@@ -9,10 +10,7 @@ var card = new UI.Card({
   scrollable: true
 });
 
-ajax({
-  url: 'http://api.nytimes.com/svc/news/v3/content/all.json?api-key=9b37b8880161ffb08f95df55513f0ce1:6:73910200',
-  type: 'json'
-}, function(data) {
+var parser = function(data) {
   var i = 0;
   card.on('click', function() {
     i += 1;
@@ -24,8 +22,23 @@ ajax({
     card.title(data.results[i].title);
     card.body(data.results[i].abstract);
   });
+  // display without any clicking,
+  // so we don't start on a blank screen
   card.title(data.results[i].title);
   card.body(data.results[i].abstract);
+};
+
+
+ajax({
+  url: 'http://api.nytimes.com/svc/news/v3/content/all.json?api-key=9b37b8880161ffb08f95df55513f0ce1:6:73910200',
+  type: 'json'
+}, parser);
+
+card.on('longClick', 'down', function() {
+  ajax({
+    url: 'http://api.nytimes.com/svc/news/v3/content/all.json?api-key=9b37b8880161ffb08f95df55513f0ce1:6:73910200',
+    type: 'json'
+  }, parser);
 });
 
 card.show();
