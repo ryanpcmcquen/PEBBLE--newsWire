19121ac44e6ee21f9c031db791b467121c88c315
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2016-01-05 07:49:00 -0800

    1.3

--- src/app.js
+++ src/app.js
@@ -1,54 +1,58 @@
-/* newsWire by ryanpcmcquen v1.2 */
-
+/* newsWire by ryanpcmcquen v1.3 */
 /*global require*/
 /*jslint browser:true, white:true*/
-
 // declare all variables
 var UI = require('ui');
 var ajax = require('ajax');
-var card = new UI.Card({
-  scrollable: true
-});
-var parser = function(data) {
-  var i = 0;
-  var cardUpdate = function() {
-    card.title(data.results[i].title);
-    card.body(data.results[i].abstract);
+var Accel = require('ui/accel');
+
+// only require statements need to be outside the closure
+(function () {
+  'use strict';
+  var card = new UI.Card({
+    scrollable: true
+  });
+  var parser = function (data) {
+    var i = 0;
+    var cardUpdate = function () {
+      card.subtitle(data.results[i].title);
+      card.body(data.results[i].abstract);
+    };
+    card.on('click', function () {
+      i += 1;
+      cardUpdate();
+    });
+    card.on('longClick', function () {
+      i -= 1;
+      cardUpdate();
+    });
+    // display without any clicking,
+    // so we don't start on a blank screen
+    cardUpdate();
   };
-  card.on('click', function() {
-    i += 1;
-    cardUpdate();
-  });
-  card.on('longClick', function() {
-    i -= 1;
-    cardUpdate();
-  });
-  // display without any clicking,
-  // so we don't start on a blank screen
-  cardUpdate();
-};
-var wires = {
-  url: [
-    'http://api.nytimes.com/svc/topstories/v1/magazine.json?api-key=dc0d0ec09ff8aa4042db01d00bf6791a:3:73910200',
-    'http://api.nytimes.com/svc/news/v3/content/all.json?api-key=9b37b8880161ffb08f95df55513f0ce1:6:73910200',
-    'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=9d29f172255680eda7a73565c0d1e52f:18:73910200'
-  ],
-  type: 'json'
-};
-var urlIterator = 0;
-var ajaxCaller = function() {
-  ajax({
-    url: wires.url[urlIterator],
-    type: wires.type
-  }, parser);
-};
+  var wires = {
+    url: [
+      'http://api.nytimes.com/svc/topstories/v1/magazine.json?api-key=dc0d0ec09ff8aa4042db01d00bf6791a:3:73910200',
+      'http://api.nytimes.com/svc/news/v3/content/all.json?api-key=9b37b8880161ffb08f95df55513f0ce1:6:73910200',
+      'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=9d29f172255680eda7a73565c0d1e52f:18:73910200'
+    ],
+    type: 'json'
+  };
+  var urlIterator = 0;
+  var ajaxCaller = function () {
+    ajax({
+      url: wires.url[urlIterator],
+      type: wires.type
+    }, parser);
+  };
 
 
-// this is the actual program
-ajaxCaller();
-card.on('longClick', function() {
-  urlIterator += 1;
+  // this is the actual program
   ajaxCaller();
-});
+  Accel.on('tap', function () {
+    urlIterator += 1;
+    ajaxCaller();
+  });
 
-card.show();
+  card.show();
+}());
